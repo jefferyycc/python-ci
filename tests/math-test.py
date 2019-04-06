2c750ce813858e27c8d8ada284238641045c5460
jefferyycc <jefferyycc@gmail.com>
2019-04-06 16:59:41 -0700

    fix cond in the math-test.py

--- tests/math-test.py
+++ tests/math-test.py
@@ -6,4 +6,4 @@ class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
         # Insert incorrect indent
-         self.assertEqual(Math.addition(3, 4), 8)
+         self.assertEqual(Math.addition(3, 4), 7)
