b6aced6ea6141b97945411aa2fe3998dee0b5475
jefferyycc <jefferyycc@gmail.com>
2019-04-14 02:02:13 -0700

    update correct answer

--- tests/math-test.py
+++ tests/math-test.py
@@ -2,8 +2,10 @@ import unittest
 import pytest
 from src.math import Math
 
+
 class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
         # Insert incorrect indent
-         self.assertEqual(Math.addition(3, 4), 8)
+        self.assertEqual(Math.addition(3, 4), 7)
+        # self.assertEqual(Math.addition(3, 4), 8)
