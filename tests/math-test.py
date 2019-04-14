1a71c027a1980c5e8ada274be6bfd0af5a4b1e4b
jefferyycc <jefferyycc@gmail.com>
2019-04-14 03:02:48 -0700

    test

--- tests/math-test.py
+++ tests/math-test.py
@@ -7,5 +7,5 @@ class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
         # Insert incorrect indent
-        self.assertEqual(Math.addition(3, 4), 7)
-        # self.assertEqual(Math.addition(3, 4), 8)
+        # self.assertEqual(Math.addition(3, 4), 7)
+        self.assertEqual(Math.addition(3, 4), 8)
