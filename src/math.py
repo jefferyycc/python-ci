562124f34feb8ccf9adfb849f4a163e1d1a3eebe
jefferyycc <jefferyycc@gmail.com>
2019-04-23 15:04:32 -0700

    test subtraction function
    also fix the number_types duplication in math.py

--- src/math.py
+++ src/math.py
@@ -1,21 +1,27 @@
 class Math(object):
-    def increment(x):
-        number_types = (int, float, complex)
-        if isinstance(x, number_types):
+    def __init__(self):
+        self.number_types = (int, float, complex)
+
+    def increment(self, x):
+        if isinstance(x, self.number_types):
             return x + 1
         else:
             raise ValueError
 
-    def decrement(x):
-        number_types = (int, float, complex)
-        if isinstance(x, number_types):
+    def decrement(self, x):
+        if isinstance(x, self.number_types):
             return x - 1
         else:
             raise ValueError
 
-    def addition(x, y):
-        number_types = (int, float, complex)
-        if isinstance(x, number_types) and isinstance(y, number_types):
+    def addition(self, x, y):
+        if isinstance(x, self.number_types) and isinstance(y, self.number_types):
             return x + y
+        else:
+            raise ValueError
+
+    def subtraction(self, x, y):
+        if isinstance(x, self.number_types) and isinstance(y, self.number_types):
+            return x - y
         else:
             raise ValueError
--- tests/math-test.py
+++ tests/math-test.py
@@ -3,28 +3,35 @@ import unittest
 from src.math import Math
 
 class MathTest(unittest.TestCase):
+    def setUp(self):
+        self.Math = Math()
+
     def test_increment_returns_correct_result(self):
-        self.assertEqual(Math.increment(3), 4)
+        self.assertEqual(self.Math.increment(3), 4)
 
     def test_increment_returns_error_message_if_args_not_numbers(self):
-        self.assertRaises(ValueError, Math.increment, 'three')
+        self.assertRaises(ValueError, self.Math.increment, 'three')
 
     def test_decrement_returns_correct_result(self):
-        self.assertEqual(Math.decrement(3), 2)
+        self.assertEqual(self.Math.decrement(3), 2)
 
     def test_decrement_returns_error_message_if_args_not_numbers(self):
-        self.assertRaises(ValueError, Math.decrement, 'three')
+        self.assertRaises(ValueError, self.Math.decrement, 'three')
 
     def test_addition_returns_correct_result(self):
-        result = Math.addition(1, 2)
+        result = self.Math.addition(1, 2)
         self.assertEqual(3, result)
 
     def test_addition_returns_error_message_if_both_args_not_numbers(self):
-        self.assertRaises(ValueError, Math.addition, 'one', 'two')
+        self.assertRaises(ValueError, self.Math.addition, 'one', 'two')
 
     def test_addition_returns_error_message_if_x_args_not_numbers(self):
-        self.assertRaises(ValueError, Math.addition, '1', 'two')
+        self.assertRaises(ValueError, self.Math.addition, '1', 'two')
 
     def test_addition_returns_error_message_if_y_args_not_numbers(self):
-        self.assertRaises(ValueError, Math.addition, 'one', '2')
+        self.assertRaises(ValueError, self.Math.addition, 'one', '2')
+
+    def test_subtraction_returns_correct_result(self):
+        result = self.Math.subtraction(5, 2)
+        self.assertEqual(3, result)
 
