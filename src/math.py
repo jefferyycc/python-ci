873aca2647b2ce2d3596898a86cd285ff2c725ea
jefferyycc <jefferyycc@gmail.com>
2019-04-23 14:33:02 -0700

    test addition function with invalid input

--- src/math.py
+++ src/math.py
@@ -6,4 +6,10 @@ class Math(object):
         return self - 1
 
     def addition(x, y):
+        number_types = (int, float, complex)
+
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            return x + y
+        else:
+            raise ValueError
         return x + y
--- tests/math-test.py
+++ tests/math-test.py
@@ -6,10 +6,19 @@ class MathTest(unittest.TestCase):
     def test_increment_returns_correct_result(self):
         self.assertEqual(Math.increment(3), 4)
 
-
     def test_decrement_returns_correct_result(self):
         self.assertEqual(Math.decrement(3), 2)
 
     def test_addition_returns_correct_result(self):
         result = Math.addition(1, 2)
         self.assertEqual(3, result)
+
+    def test_addition_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, Math.addition, 'one', 'two')
+
+    def test_addition_returns_error_message_if_x_args_not_numbers(self):
+        self.assertRaises(ValueError, Math.addition, '1', 'two')
+
+    def test_addition_returns_error_message_if_y_args_not_numbers(self):
+        self.assertRaises(ValueError, Math.addition, 'one', '2')
+
